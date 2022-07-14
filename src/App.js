921bc06195174a685dbf07b4e4b31343666e5ad0
Daniel Yabu <danielyabu@yahoo.com>
2022-07-14 17:23:22 -0300

    Projeto iniciado

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <h1>Iniciando o projeto App de Receitas</h1>
     </div>
   );
 }
