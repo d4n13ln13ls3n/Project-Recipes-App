c9a2dc90ef1d5fdf3097ac51db6c01ba99f644a3
Almir-Araujo <araujo.almir@gmail.com>
2022-07-14 18:09:03 -0300

    Requisito 2 OK

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
 
 function App() {
   return (
