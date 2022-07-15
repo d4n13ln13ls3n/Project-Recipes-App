8baee209250413b72f0aa933ea5afc3278a36f12
kikutii <joaovictorkikuti@gmail.com>
2022-07-15 19:22:04 -0300

    adicionado rota de comidas e bebidas

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/drinks" component={ Drinks } />
     </Switch>
   );
 }
