68b2d769b288acac07e01f9a218a661a4c7e0c64
Almir-Araujo <araujo.almir@gmail.com>
2022-07-15 16:09:50 -0300

    Mudança de nome da pasta hook para hooks

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Farewell, front-end', () => {
+test('Testa o componente Login e suas funcionalidades', () => {
   render(<App />);
   const linkElement = screen.getByText(/TRYBE/i);
   expect(linkElement).toBeInTheDocument();
