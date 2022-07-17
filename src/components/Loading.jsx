20d895ec1c70bc3812b4c59ad51adf59d7463cc6
kikutii <joaovictorkikuti@gmail.com>
2022-07-17 01:28:35 -0300

    adicionado e refatorado componente de loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import loadingGif from '../images/loading.gif';
+import '../css/Loading.css';
+
+// esse é o nosso componente de loading, será executado quando a requisição estiver em promise
 
 export default function Loading() {
   return (
-    <h3>Loading...</h3>
+    <img
+      src={ loadingGif }
+      alt="loading"
+      className="loading"
+    />
   );
 }
