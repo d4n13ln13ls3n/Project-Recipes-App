6394699f9e8e867c91bc75c782aa4e8bb6694cde
kikutii <joaovictorkikuti@gmail.com>
2022-07-17 01:31:23 -0300

    trocado de diretório de hooks para context e RecipesAppProvider refatorado

---
+++ src/context/RecipesAppProvider.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import RecipesAppContext from './RecipesAppContext';
+import useFoodsAndDrinks from '../hooks/useFoodsAndDrinks';
+import useFilter from '../hooks/useFilter';
+
+export default function RecipesAppProvider({ children }) {
+  const [login, setLogin] = useState({ email: '', password: '' });
+  const [filters, setFilters] = useState({ recipesLimit: 12 });
+
+  // hooks costumizados, o useFoodsAndDrinks ele faz a requisição das comidas e bebidas
+  // e o useFilter ele filtra as comidas e bebidas basiadas na seleção do usuário
+  const [foods, drinks] = useFoodsAndDrinks();
+  const [filteredFoods, filteredDrinks] = useFilter(foods, drinks, filters);
+
+  const context = {
+    login,
+    setLogin,
+    foods,
+    filteredFoods,
+    drinks,
+    filteredDrinks,
+    filters,
+    setFilters,
+  };
+
+  return (
+    <RecipesAppContext.Provider value={ context }>
+      { children }
+    </RecipesAppContext.Provider>
+  );
+}
+
+RecipesAppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
