e6a4719a6579ec7ce7fd9a12bd7cea3d03cb4cbe
Daniel Yabu <danielyabu@yahoo.com>
2022-07-18 18:58:16 -0300

    Requisito 10 concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import searchIcon from '../images/searchIcon.svg';
 
 function SearchBar() {
   return (
@@ -6,6 +7,39 @@ function SearchBar() {
       <form>
         <label htmlFor="search-input">
           <input data-testid="search-input" name="search-input" />
+        </label>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+        >
+          <img src={ searchIcon } alt="search" />
+        </button>
+        <label htmlFor="ingredient" name="ingredient">
+          <input
+            type="radio"
+            id="ingredient"
+            name="ingredient"
+            data-testid="ingredient-search-radio"
+          />
+          Ingredient
+        </label>
+        <label htmlFor="name" name="name">
+          <input
+            type="radio"
+            name="name"
+            id="name"
+            data-testid="name-search-radio"
+          />
+          Name
+        </label>
+        <label htmlFor="first-letter" name="first-letter">
+          <input
+            type="radio"
+            name="first-letter"
+            id="first-letter"
+            data-testid="first-letter-search-radio"
+          />
+          First Letter
         </label>
       </form>
     </div>
