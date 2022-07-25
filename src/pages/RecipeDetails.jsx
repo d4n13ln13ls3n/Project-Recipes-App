930d1b771cde0ada02a4135fbba59564a551313c
Lucas De Medeiros Luz <lucasdmluz@hotmail.com>
2022-07-25 02:15:04 -0300

    requisito 27 Implemente os 6 cards de recomendação e o requisito 28 Desenvolva um botão de nome "Start Recipe" que deve ficar fixo na parte de baixo da tela

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -5,6 +5,7 @@ import fetchRecipeDetails from '../services/fetchRecipeDetails';
 import fetchFood from '../services/fetchFood';
 import fetchDrink from '../services/fetchDrink';
 import arrayIngredientsMeasure from '../services/arrayIngredientsMeasure';
+import '../css/recipeDetails.css';
 
 function RecipeDetails() {
   const [recipe, setRecipe] = useState();
@@ -12,18 +13,35 @@ function RecipeDetails() {
   const [recipeMeasure, setRecipeMeasure] = useState();
   const [recommendationFood, setRecommendationFood] = useState([]);
   const [recommendationDrinks, setRecommendationDrinks] = useState([]);
+  const [filterRecommendation, setFilterRecommendation] = useState([]);
   const history = useHistory();
   const { location: { pathname } } = history;
   const id = useParams();
+
   useEffect(() => {
     const storeRecipe = async () => {
       if (pathname === `/foods/${id.id}`) {
         setRecommendationDrinks(await fetchDrink([]));
-      } else { setRecommendationFood(await fetchFood([])); }
+      } else {
+        setRecommendationFood(await fetchFood([]));
+      }
       setRecipe(await fetchRecipeDetails(id.id, pathname));
     };
     storeRecipe();
   }, []);
+
+  useEffect(() => {
+    if (recommendationDrinks !== [] || recommendationFood !== []) {
+      if (pathname === `/foods/${id.id}`) {
+        const max = 6;
+        setFilterRecommendation(recommendationDrinks.filter((_, index) => index < max));
+      } else {
+        const max = 6;
+        setFilterRecommendation(recommendationFood.filter((_, index) => index < max));
+      }
+    }
+  }, []);
+
   function filterIngredient() {
     const ingredients = arrayIngredientsMeasure.ingredients.map((ingredient) => (
       recipe[ingredient]
@@ -31,7 +49,15 @@ function RecipeDetails() {
     const remove = ingredients.filter((ingredient) => (pathname === `/foods/${id.id}`
       ? (ingredient !== '') : (ingredient !== null && ingredient !== undefined)));
     setRecipeIngredient(remove);
+    if (pathname === `/foods/${id.id}`) {
+      const max = 6;
+      setFilterRecommendation(recommendationDrinks.filter((_, index) => index < max));
+    } else {
+      const max = 6;
+      setFilterRecommendation(recommendationFood.filter((_, index) => index < max));
+    }
   }
+
   function filterMeasure() {
     const ingredients = arrayIngredientsMeasure.measure.map((ingredient) => (
       recipe[ingredient]
@@ -39,13 +65,17 @@ function RecipeDetails() {
     const remove = ingredients.filter((ingredient) => ingredient !== ' ');
     setRecipeMeasure(remove);
   }
+
   useEffect(() => {
     if (recipe !== undefined) {
-      // console.log(recipe, recommendationFood, recommendationDrinks);
+      console.log(recipe);
       filterIngredient();
       filterMeasure();
     }
   }, [recipe]);
+
+  console.log(filterRecommendation);
+
   return (
     <div>
       {
@@ -88,12 +118,43 @@ function RecipeDetails() {
                     data-testid="video"
                     src={ recipe.strYoutube }
                   />)}
-              <div
-                data-testid="0-recomendation-card"
-              />
-              Essa vai ser a recomendação
+              <div className="recomendations">
+                {
+                  filterRecommendation.map((e, index) => (
+                    <a
+                      href={ pathname === `/foods/${id.id}`
+                        ? (`/drinks/${e.idDrink}`) : (`/foods/${e.idMeal}`) }
+                      key={ e.idDrink || e.idMeal }
+                      data-testid={ `${index}-recomendation-card` }
+                      className="recommendationCards"
+                    >
+                      <img
+                        src={ pathname === `/foods/${id.id}`
+                          ? (e.strDrinkThumb) : (e.strMealThumb) }
+                        alt={ pathname === `/foods/${id.id}`
+                          ? (e.strDrink) : (e.strMeal) }
+                        data-testid={ `${index}-recomendation-image` }
+                        className="recommendationImage"
+                      />
+                      <strong
+                        data-testid={ `${index}-recomendation-title` }
+                      >
+                        { pathname === `/foods/${id.id}`
+                          ? (e.strDrink) : (e.strMeal) }
+                      </strong>
+                    </a>
+                  ))
+                }
+              </div>
               <br />
-              <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+              <br />
+              <button
+                type="button"
+                data-testid="start-recipe-btn"
+                id="start-recipe-btn"
+              >
+                Start Recipe
+              </button>
             </div>
           ) : (
             <p>...</p>
--- src/services/fetchRecipeDetails.jsx
+++ src/services/fetchRecipeDetails.jsx
@@ -3,11 +3,11 @@ export default async function apiRecipeDetails(id, pathname) {
     const receita = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
     const response = await fetch(receita);
     const { meals } = await response.json();
-    return meals;
+    return meals[0];
   } if (pathname === `/drinks/${id}`) {
     const receita = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
     const response = await fetch(receita);
     const { drinks } = await response.json();
-    return drinks;
+    return drinks[0];
   }
 }
