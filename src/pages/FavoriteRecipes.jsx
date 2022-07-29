1a4b1be3ef5ba7afc2f5a6aa9d9198730a484c3c
Almir-Araujo <araujo.almir@gmail.com>
2022-07-29 18:35:53 -0300

    Correção erro requisito 7 - OK

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import FavoriteRecipeContainer from '../components/FavoriteRecipesContainer';
 import '../css/FavoriteRecipes.css';
